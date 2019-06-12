7ade7f0e870e4d059826a4df5bfaa9a96d8356f0
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-12 19:21:14 +0200

    toString() refactored

--- examenJava/src/main/java/org/lasencinas/packing/objetos/Caja.java
+++ examenJava/src/main/java/org/lasencinas/packing/objetos/Caja.java
@@ -21,4 +21,9 @@ public class Caja implements Packing {
     public String envoltorio() {
         return envoltorio;
     }
+
+    @Override
+    public String toString() {
+        return "caja";
+    }
 }

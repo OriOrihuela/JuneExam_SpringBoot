0ae73708cb4de05d8148f93c0dfb7d81f0c5e2a3
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-12 18:30:37 +0200

    property of type Packing deleted and toString() refactored.

--- examenJava/src/main/java/org/lasencinas/item/cereales/Cereales.java
+++ examenJava/src/main/java/org/lasencinas/item/cereales/Cereales.java
@@ -5,16 +5,11 @@ import org.lasencinas.packing.Packing;
 
 public abstract class Cereales implements Item {
 
-    /* ---- PROPERTIES OF THE CLASS ---- */
-    private Packing caja = null;
-
     /* ---- MAIN BEHAVIOURS ---- */
     public abstract Packing empaquetado();
 
     @Override
     public String toString() {
-        return "Cereales{" +
-                "caja: " + caja +
-                '}';
+        return "Cereales{}";
     }
 }

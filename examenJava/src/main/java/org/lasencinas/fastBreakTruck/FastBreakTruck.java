6fc44150a36fb3f0bbdaa23f92a2dd445b2a1812
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-12 19:22:24 +0200

    main methods done to pass th App.Java main.

--- examenJava/src/main/java/org/lasencinas/fastBreakTruck/FastBreakTruck.java
+++ examenJava/src/main/java/org/lasencinas/fastBreakTruck/FastBreakTruck.java
@@ -1,39 +1,40 @@
 package org.lasencinas.fastBreakTruck;
 
 import org.lasencinas.desayuno.Desayuno;
+import org.lasencinas.item.Item;
+import org.lasencinas.item.bebida.Bebida;
 import org.lasencinas.item.bebida.subtipos.FleebJuice;
 import org.lasencinas.item.bebida.subtipos.TurbulentJuice;
+import org.lasencinas.item.cereales.Cereales;
 import org.lasencinas.item.cereales.subtipos.Eyeholes;
 import org.lasencinas.item.cereales.subtipos.Smiggles;
+import org.lasencinas.packing.Packing;
+import org.lasencinas.packing.objetos.Caja;
+import org.lasencinas.packing.objetos.Tubo;
+
+import java.util.HashSet;
+import java.util.Set;
 
 public class FastBreakTruck {
 
-    /* ---- PROPERTIES OF THE CLASS ---- */
-    private Desayuno desayuno = null;
-
-
-    /* ---- GETTERS ---- */
-    public Desayuno getDesayuno() {
+    /* ---- MAIN BEHAVIOURS ---- */
+    public static Desayuno prepararEyeHoles() {
+        Cereales eyeHoles = new Eyeholes(new Caja());
+        Bebida turbulentJuice = new TurbulentJuice(new Tubo());
+        Set<Item> items = new HashSet<>();
+        items.add(eyeHoles);
+        items.add(turbulentJuice);
+        Desayuno desayuno = new Desayuno(items);
         return desayuno;
     }
 
-
-    /* ---- SETTERS ---- */
-    public void setDesayuno(Desayuno desayuno) {
-        this.desayuno = desayuno;
-    }
-
-    /* ---- CONSTRUCTORS ---- */
-    public FastBreakTruck() {
-    }
-
-
-    /* ---- MAIN BEHAVIOURS ---- */
-    public static Desayuno prepararEyeHoles() {
-        return null;
-    }
-
     public static Desayuno prepararSmiggles() {
-        return null;
+        Cereales smiggles = new Smiggles(new Caja());
+        Bebida fleebJuice = new FleebJuice(new Tubo());
+        Set<Item> items = new HashSet<>();
+        items.add(smiggles);
+        items.add(fleebJuice);
+        Desayuno desayuno = new Desayuno(items);
+        return desayuno;
     }
 }

6a25d51193c1edc75278c28b099a3d3a2541a6fb
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-12 18:33:12 +0200

    Properties added (Packing and float type); constructor with new String parameter; Getters refactored as well.

--- examenJava/src/main/java/org/lasencinas/item/bebida/subtipos/TurbulentJuice.java
+++ examenJava/src/main/java/org/lasencinas/item/bebida/subtipos/TurbulentJuice.java
@@ -2,27 +2,33 @@ package org.lasencinas.item.bebida.subtipos;
 
 import org.lasencinas.item.bebida.Bebida;
 import org.lasencinas.packing.Packing;
+import org.lasencinas.packing.objetos.Tubo;
 
 public class TurbulentJuice extends Bebida {
 
+    /* ---- PROPERTIES ---- */
+    private Packing tubo = null;
+    private float precio = 30;
+
     /* ---- CONSTRUCTORS ---- */
-    public TurbulentJuice() {
+    public TurbulentJuice(Packing tubo) {
+        this.tubo = new Tubo("TurbulentJuice");
     }
 
 
     /* ---- MAIN BEHAVIOURS ----- */
     @Override
     public String nombre() {
-        return null;
+        return tubo.envoltorio();
     }
 
     @Override
     public Packing empaquetado() {
-        return null;
+        return tubo;
     }
 
     @Override
     public Float pvp() {
-        return null;
+        return precio;
     }
 }

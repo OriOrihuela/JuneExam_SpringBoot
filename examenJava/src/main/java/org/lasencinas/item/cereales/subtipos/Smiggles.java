641ebc906b98b6b7dc16ad818f7a9f0a266ada88
Eduardo Orihuela Verdugo <eduardo.orihuela.4e@gmail.com>
2019-06-12 18:32:02 +0200

    New propertie sadded (Packing and float type), constructor refactored. Getters as well.

--- examenJava/src/main/java/org/lasencinas/item/cereales/subtipos/Smiggles.java
+++ examenJava/src/main/java/org/lasencinas/item/cereales/subtipos/Smiggles.java
@@ -1,27 +1,42 @@
 package org.lasencinas.item.cereales.subtipos;
 
+
 import org.lasencinas.item.cereales.Cereales;
 import org.lasencinas.packing.Packing;
+import org.lasencinas.packing.objetos.Caja;
 
 public class Smiggles extends Cereales {
 
+    /* ---- PROPERTIES ---- */
+    private Packing caja = null;
+    private float precio = 50;
+
     /* ---- CONSTRUCTOR ---- */
-    public Smiggles() {
+    public Smiggles(Packing caja) {
+        this.caja = new Caja("Smiggles");
     }
 
     /* ---- MAIN BEHAVIOURS ---- */
     @Override
     public String nombre() {
-        return null;
+        return caja.envoltorio();
     }
 
     @Override
     public Packing empaquetado() {
-        return null;
+        return caja;
     }
 
     @Override
     public Float pvp() {
-        return null;
+        return precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Smiggles{" +
+                "caja=" + caja +
+                ", precio=" + precio +
+                "} " + super.toString();
     }
 }
